dd6e19eece6a67b44eac4336241a3527de5400e0
Wellington Pereira <wellsouza24@gmail.com>
2024-07-03 13:50:38 -0300

    commit: test

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,10 +7,12 @@ import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<DefaultLayout/>}>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
       </Route>
     </Routes>
   )
 }
+
+// commit teste
